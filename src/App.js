8ad87b21ea551b47b8ad0e2b8e4a38b2fc5e78ba
Nadaarina <Nadaarina.nadaarinacs04@gmail.com>
2026-03-25 22:50:34 +0700

    Update tampilan pakai Tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-blue-500 min-h-screen flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold p-10 bg-black rounded-lg">
+        Tugas Kelompok 4 - Berhasil Pakai Tailwind!
+      </h1>
     </div>
   );
 }
