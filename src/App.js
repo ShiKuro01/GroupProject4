d4ae1e9ec42daca705a3a34dea1e4b8568b791a1
ShiKuro01 <mochazriel1406@gmail.com>
2026-04-01 08:14:51 +0700

    update on main page

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ const Home = () => {
       
       {/* Header Section */}
       <h1 className="text-white text-4xl md:text-5xl font-extrabold p-8 bg-black/90 rounded-2xl text-center mb-12 shadow-2xl border-b-4 border-blue-400">
-        Tugas Kelompok 4
+        Group 4 Task
         <span className="block text-xl md:text-2xl text-blue-400 mt-3 font-medium">
-          Berhasil Pakai Tailwind! 🎉
+          Completed with tailwind!
         </span>
       </h1>
       
@@ -23,19 +23,19 @@ const Home = () => {
           to="/azriel" 
           className="px-10 py-4 bg-white text-blue-600 font-bold rounded-full shadow-xl hover:bg-gray-100 hover:scale-105 transition-transform duration-300 text-xl text-center border-2 border-transparent hover:border-blue-300"
         >
-          Profil Azriel
+          Azriel Profile
         </Link>
         <Link 
           to="/naufal" 
           className="px-10 py-4 bg-white text-blue-600 font-bold rounded-full shadow-xl hover:bg-gray-100 hover:scale-105 transition-transform duration-300 text-xl text-center border-2 border-transparent hover:border-blue-300"
         >
-          Profil Naufal
+          Naufal Profile
         </Link>
         <Link 
           to="/nada" 
           className="px-10 py-4 bg-white text-blue-600 font-bold rounded-full shadow-xl hover:bg-gray-100 hover:scale-105 transition-transform duration-300 text-xl text-center border-2 border-transparent hover:border-blue-300"
         >
-          Profil Nada
+          Nada Profile
         </Link>
       </div>
 
